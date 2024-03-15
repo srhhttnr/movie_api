fab3670fe0331e4a5181c25c965c97450143027d
srhhttnr <shuettner12@gmail.com>
2024-03-15 06:42:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -305,7 +305,7 @@ app.delete('/users/:Username/movies/:MovieID', async (req, res) => {
 
 //DELETE user from registered users
 app.delete('/users/:Username', async (req, res) => {
-    await Users.findOneAndRemove({ Username: req.params.Username })
+    await Users.findOneAndDelete({ Username: req.params.Username })
       .then((user) => {
         if (!user) {
           res.status(400).send(req.params.Username + ' was not found');
